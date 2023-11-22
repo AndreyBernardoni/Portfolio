64c2b08dd5c7a9965067dc0d55a4baf0320a1bfb
Andrey Bernardoni <fialho.andrey2017@gmail.com>
2023-11-22 12:31:10 -0300

    Transition Implemented

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ecb8c3919247bf3a"; }
+/******/ 		__webpack_require__.h = function() { return "c5ce6f993fb230a3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
